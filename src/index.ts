ddc2ea963bd54201f8c995a4b68cfd64e8114151
Patrick Zhang <patrickjyzhang@gmail.com>
2021-11-14 15:35:29 -0800

    add body parser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import groupRoutes from './api/groups';
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('API running');
